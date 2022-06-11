009d6d4e24eb56564409efac5c7c68282c69856b
Yuvraj Singh Chauhan <81570327+ng3w-uv@users.noreply.github.com>
2022-06-11 11:55:55 +0530

    Time: 156 ms (55.09%), Space: 24.6 MB (44.49%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    void expand(int &l, int &r, string &s, string &result,int &resultLength){
+        while(l>=0 && r < s.size() && s[l]==s[r]){
+                if((r-l+1)>resultLength){
+                    result = s.substr(l,(r-l+1));
+                    resultLength = r-l+1;
+                }
+                l -= 1;
+                r += 1;
+            }
+    }
     string longestPalindrome(string s) {
         int resultLength = 0;
         string result = "";
@@ -10,26 +20,28 @@ public:
             //we are using expand from the middle char concept
             //checking for odd length
             int l = i,r = i;
-            while(l>=0 && r < n && s[l]==s[r]){
-                if((r-l+1)>resultLength){
-                    result = s.substr(l,(r-l+1));
-                    resultLength = r-l+1;
-                }
-                l -= 1;
-                r += 1;
-            }
+            expand(l,r,s,result,resultLength);
+            // while(l>=0 && r < n && s[l]==s[r]){
+            //     if((r-l+1)>resultLength){
+            //         result = s.substr(l,(r-l+1));
+            //         resultLength = r-l+1;
+            //     }
+            //     l -= 1;
+            //     r += 1;
+            // }
             
             //checking for even length
             l = i;
             r = i+1;
-            while(l>=0 && r<n && s[l]==s[r]){
-                if((r-l+1)>resultLength){
-                    result = s.substr(l,(r-l+1));
-                    resultLength = r-l+1;
-                }
-                l -= 1;
-                r += 1;
-            }
+            expand(l,r,s,result,resultLength);
+            // while(l>=0 && r<n && s[l]==s[r]){
+            //     if((r-l+1)>resultLength){
+            //         result = s.substr(l,(r-l+1));
+            //         resultLength = r-l+1;
+            //     }
+            //     l -= 1;
+            //     r += 1;
+            // }
             
         }
         return result;
